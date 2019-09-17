165cc65065e0bc66fda4f930bfa5dc1e93b0e0d8
NilsNaegele <nils.holger.naegele@gmail.com>
2019-09-17 22:09:41 +0200

    stop watch 1

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { CoreRoutingModule } from './core-routing.module';
 import { HomeComponent } from './components/home/home.component';
 import { SharedModule } from '../shared/shared.module';
+import { TimerComponent } from './components/timer/timer.component';
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, TimerComponent],
   imports: [
     SharedModule,
     CoreRoutingModule
