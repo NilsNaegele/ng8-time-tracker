c6f1190c66f27cd6e15e05194fc5bff316a75c9d
NilsNaegele <nils.holger.naegele@gmail.com>
2019-09-24 20:36:33 +0200

    hall of fame

--- src/app/modules/core/components/deadline-box/hall-of-fame-entry/hall-of-fame-entry.component.ts
+++ src/app/modules/core/components/deadline-box/hall-of-fame-entry/hall-of-fame-entry.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { EventModel } from '../models/event-model';
 
 @Component({
   selector: 'app-hall-of-fame-entry',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hall-of-fame-entry.component.scss']
 })
 export class HallOfFameEntryComponent implements OnInit {
+  @Input() event: EventModel;
+
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
 
   constructor() { }
 
   ngOnInit() {
+    this.calculateTimeDifference();
+  }
+
+  calculateTimeDifference(): void {
+    let timeRemaining = this.event.dueDate.getTime() + this.event.dueDate.getTimezoneOffset() - Date.now();
+    timeRemaining += 1000 * 60 * 60 * 24;
+    timeRemaining -= 1000 * 60 * 60;
+
+    this.seconds = Math.floor((timeRemaining / 1000) % 60);
+    this.minutes = Math.floor((timeRemaining / 1000 / 60) % 60);
+    this.hours = Math.floor((timeRemaining / (1000 * 60 * 60)) % 24);
+    this.days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
   }
 
 }
--- src/app/modules/core/components/deadline-box/hall-of-fame/hall-of-fame.component.ts
+++ src/app/modules/core/components/deadline-box/hall-of-fame/hall-of-fame.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { EventModel } from '../models/event-model';
+import { EventsService } from '../services/events.service';
 
 @Component({
   selector: 'app-hall-of-fame',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HallOfFameComponent implements OnInit {
 
-  constructor() { }
+  events: EventModel[] = [];
 
-  ngOnInit() {
+  constructor(private eventsService: EventsService) { }
+
+  async ngOnInit() {
+    this.events = await this.eventsService.getAllCountdownEvents();
+    this.events.sort((a: EventModel, b: EventModel) => {
+      if (a.dueDate < b.dueDate) {
+        return 11;
+      }
+      if (a.dueDate > b.dueDate) {
+        return -1;
+      }
+      return 0;
+    });
   }
 
 }
