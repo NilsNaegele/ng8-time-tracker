20bd8b7e7b3586e8fab05735c3d1fb70d1cbafda
NilsNaegele <nils.holger.naegele@gmail.com>
2019-10-05 08:11:33 +0200

    dashboard helpers

--- src/app/modules/track-my-time/features/dashboard/utils/graph-data.ts
+++ src/app/modules/track-my-time/features/dashboard/utils/graph-data.ts
@@ -1,2 +1,35 @@
-export class GraphData {
-}
+import { combineLatest, Observable } from 'rxjs';
+
+import { HistoryGrouping } from '../../../shared/models/history.model';
+import { GraphDataItem } from '../models/graph';
+
+import { filterGroupingsByDateRange } from '../../../shared/utils/history.utils';
+import { mapToGraphData, padDateGraphData, sortGraphDataByValue } from './graph';
+
+type GraphDataModifier = (items: GraphDataItem[], dates: Date[]) => GraphDataItem[];
+
+export const getGraphData =
+  (groupings: Observable<HistoryGrouping[]>,
+   dateRange: Observable<Date[]>,
+   modifier: GraphDataModifier = x => x): Observable<GraphDataItem[]> => {
+    return combineLatest(groupings, dateRange, (groups, dates) => {
+      const groupingsToGraph = filterGroupingsByDateRange(groups, dates);
+      const graphItems = mapToGraphData(groupingsToGraph);
+      return modifier(graphItems, dates);
+    });
+  };
+
+export const getPaddedGraphData =
+  (groupings: Observable<HistoryGrouping[]>, dateRange: Observable<Date[]>): Observable<GraphDataItem[]> => {
+    const modifier: GraphDataModifier = (items, dates) => {
+      const reversed = items.reverse();
+      return padDateGraphData(reversed, dates);
+    };
+    return getGraphData(groupings, dateRange, modifier);
+  };
+
+export const getSortedGraphData =
+  (groupings: Observable<HistoryGrouping[]>, dateRange: Observable<Date[]>): Observable<GraphDataItem[]> => {
+    return getGraphData(groupings, dateRange, sortGraphDataByValue);
+  };
+
--- src/app/modules/track-my-time/features/dashboard/utils/graph.ts
+++ src/app/modules/track-my-time/features/dashboard/utils/graph.ts
@@ -1,2 +1,47 @@
-export class Graph {
-}
+import { isSameDay } from 'date-fns';
+
+import { HistoryGrouping } from '../../../shared/models/history.model';
+import { GraphDataItem } from '../models/graph';
+
+import { formatDate } from '../../../shared/utils/date.utils';
+
+export const DEFAULT_KEY = 'Uncategorized';
+
+const getGraphDataItemIfInRange = (items: GraphDataItem[], rangeDate: Date): GraphDataItem | undefined => {
+    for (let i = 0; i < items.length; i++) {
+      const itemDate = new Date(items[i].name);
+      if (isSameDay(rangeDate, itemDate)) {
+        return items[i];
+      }
+    }
+    return undefined;
+  };
+
+export const mapToGraphData = (groupings: HistoryGrouping[]): GraphDataItem[] => {
+  return groupings.map(grouping => <GraphDataItem>{
+    name: grouping.key === '' ? DEFAULT_KEY : grouping.key,
+    value: grouping.totalTime
+  });
+};
+
+export const padDateGraphData = (graphItems: GraphDataItem[], dateRange: Date[]): GraphDataItem[] => {
+  const paddedGraphItems: GraphDataItem[] = [];
+  for (let i = 0; i < dateRange.length; i++) {
+    let itemToAdd = getGraphDataItemIfInRange(graphItems, dateRange[i]);
+
+    if (!itemToAdd) {
+      itemToAdd = {
+        name: formatDate(dateRange[i]),
+        value: 0
+      };
+    }
+    paddedGraphItems.push(itemToAdd);
+  }
+  return paddedGraphItems;
+};
+
+export const sortGraphDataByValue = (items: GraphDataItem[]): GraphDataItem[] => {
+  return items.sort((a, b) => b.value - a.value);
+};
+
+
